b0844cc9bf98ad5b87b30e8a5b3bf859e95d47f2
kryst <kryst@LAPTOP-CCOGS54A>
2026-05-25 08:49:05 +0200

    kosmetyczne

--- FullstackAPPProject/Controllers/KontoController.cs
+++ FullstackAPPProject/Controllers/KontoController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Projekt.Controllers;
+namespace FullstackAPPProject.Controllers;
 
 public class KontoController : Controller
 {
